d904596916e3ab58e1f2aa7a2f9e4e43abdf89ab
alexey <shaman1801@yandex.ru>
2022-09-20 10:19:43 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+   Является ли день недели выходным
+*/
+
+Console.Write("Input number (1..7) -> ");
+
+string val_str = Console.ReadLine();
+int digit = int.Parse(val_str);
+
+if((digit < 1) || (digit > 7))
+{
+    Console.WriteLine("Неверный формат ввода");
+    return;
+}
+
+if ((digit == 6) || (digit == 7)) Console.WriteLine("Да - Выходной");
+else Console.WriteLine("Нет - Рабочий день");
