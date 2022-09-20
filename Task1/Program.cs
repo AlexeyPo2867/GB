8515573d1bd5703c52472164afe498d5d97e5f7e
alexey <shaman1801@yandex.ru>
2022-09-20 10:00:31 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+   Выводит вторую цифру
+*/
+
+Console.Write("Input number -> ");
+string val_str = Console.ReadLine();
+int val = int.Parse(val_str);
+
+int digits = ((val % 100) - (val % 10)) / 10;
+
+Console.WriteLine($"Second digits: {digits}");
